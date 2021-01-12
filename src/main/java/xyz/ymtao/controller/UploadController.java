3e95b527cec93fbce21ed877e8a92e1359c182e1
zhangerheba <229115752@qq.com>
2021-01-12 14:38:13 +0800

    w

--- src/main/java/xyz/ymtao/controller/UploadController.java
+++ src/main/java/xyz/ymtao/controller/UploadController.java
@@ -3,6 +3,9 @@ package xyz.ymtao.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,14 +31,14 @@ public class UploadController {
 
     @ApiOperation(value = "上传招投标信息文件")
     @PostMapping("/uploadFile")
-    public String uploadFile(@RequestParam @ApiParam(value = "文件") MultipartFile file) throws IOException {
+    public String uploadFile(@RequestParam @ApiParam(value = "文件") MultipartFile file, @RequestParam(name = "phone") String phone) throws IOException {
         String oringinalFileName = file.getOriginalFilename();
         int index = oringinalFileName.lastIndexOf(".");
         int len = oringinalFileName.length();
         String fileType = oringinalFileName.substring(index+1, len);
         String[] fileTypes = {"log", "txt", "html"};
         if(fileType == null || fileType == "" || !Arrays.asList(fileTypes).contains(fileType.toLowerCase())){
-           return "上传文件格式错误";
+            return "上传文件格式错误";
         }
         int maxSize = 1048576 * 30;
         if(file.getSize() > maxSize){
@@ -49,8 +52,9 @@ public class UploadController {
         while((line=br.readLine()) != null){
             String[] arr = line.split("#");
             if(arr != null && arr.length > 3){
+                line = isFooter(line);
                 line = line.replaceAll(arr[0],"<div style=\"background: yellow;\">" + arr[0] + "</div>");
-                ZtbDocument document = new ZtbDocument(arr[0],arr[1],arr[2],line);
+                ZtbDocument document = new ZtbDocument(arr[0],arr[1],arr[2],line,phone);
                 try{
                     mongoTemplate.insert(document);
                 } catch (Exception e){
@@ -62,5 +66,12 @@ public class UploadController {
         reader.close();
         br.close();
         return "ok";
+    }
+
+    //去除特定样式的标签
+    public static String isFooter ( String line){
+        Document doc= Jsoup.parse(line, "UTF-8");
+        Elements select = doc.select(".footer").remove();
+        return doc.toString();
     }
 }
